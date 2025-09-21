825bde6024fbf7590c6dcd4d745dc86ef99bdc22
nauyihsnehs <65580023+nauyihsnehs@users.noreply.github.com>
2025-09-21 21:49:42 +0800

    Refactor run_sampler parameters and update model path

--- inference_pano_gen.py
+++ inference_pano_gen.py
@@ -28,22 +28,17 @@ def get_mask(mask, num_samples):
 
 def run_sampler(
         model, input_pano, input_asg, input_sg, input_img,
-        img_name, output_path, input_mask=None,
-        batch: int = 4,  # batch size for sampling
+        img_name, output_path, batch: int = 4,
         seed=-1,
         strength=1.0,
         ddim_steps=50,
-        eta=0.3,
+        eta=0.,
 ):
     with torch.no_grad():
         model = model.cuda()
         ddim_sampler = DDIMSampler(model)
-        mask = get_mask(input_mask, batch)
 
         H, W, C = input_pano.shape
-        x0 = input_pano.clone()
-        x0 = torch.stack([x0 for _ in range(batch)], dim=0)
-        x0 = einops.rearrange(x0, 'b h w c -> b c h w').clone()
 
         control = input_asg.clone()
         control = torch.stack([control for _ in range(batch)], dim=0)
@@ -66,9 +61,7 @@ def run_sampler(
         shape = (4, H // 8, W // 8)
         ddim_sampler.make_schedule(ddim_steps, ddim_eta=eta, verbose=True)
 
-        x0 = model.get_first_stage_encoding(model.encode_first_stage(x0))
-        # x0_rand = torch.randn(batch, *shape).cuda()
-        # x0 = x0 * mask + x0_rand * (1 - mask)
+        x0_rand = torch.randn(batch, *shape).cuda()
 
         model.control_scales = ([strength] * 13)
 
@@ -79,8 +72,7 @@ def run_sampler(
             conditioning=cond,
             verbose=False,
             eta=eta,
-            x0=x0,
-            mask=mask,
+            x0=x0_rand
         )
 
         x_samples = model.decode_first_stage(samples)
@@ -95,20 +87,17 @@ def run_sampler(
 
 if __name__ == '__main__':
     cmodel = create_model('./pano_gen/configs/cldm_v15_clip_asg_sg.yaml').cpu()
-    model_path = 'ckpts/control-epoch=4-step=47950.ckpt'
+    model_path = 'ckpts/control-epoch=9-step=47950.ckpt'
     epoch, step = map(int, __import__('re').search(r'epoch=(\d+)-step=(\d+)', model_path).groups())
     model_name = f'e{epoch:02d}_s{step:06d}'
-    cmodel.load_state_dict(load_state_dict(model_path, location='cuda'))
+    cmodel.load_state_dict(load_state_dict(model_path, location='cuda'), strict=False)
     cmodel.eval()
 
     input_paths = sorted([p.as_posix() for p in pathlib.Path('pano_gen/inpainting_data/inputs').glob('*.*')])
     asg_paths = 'pano_gen/inpainting_data/asg'
     sg_paths = 'pano_gen/inpainting_data/sg'
-    mask_path = 'pano_gen/outpainting-mask.png'
 
     pano_res = (512, 256)
-    mask = np.array(Image.open(mask_path).convert("L").resize(pano_res, resample=0)) / 255.0
-    in_mask = torch.from_numpy(mask).float().cuda()[..., None]
     lum_weight = np.asarray([0.2126, 0.7152, 0.0722])[None, None, ...]  # rgb
 
     save_path = f'pano_gen/inpainting_data/output/{model_name}'
@@ -117,7 +106,6 @@ if __name__ == '__main__':
 
     def to_norm_tensor(ndarray):
         tensor = torch.from_numpy(ndarray).float().cuda()
-        tensor = (tensor - tensor.min()) / (tensor.max() - tensor.min())
         return tensor * 2 - 1
 
 
@@ -126,7 +114,12 @@ if __name__ == '__main__':
         save_name = input_path.split('/')[-1].split('.')[0] + f'_{model_name}'
 
         in_img = np.array(Image.open(input_path).convert("RGB")) / 255.0
-        in_pano = pers2pano(in_img, pano_res, vfov=90, rotation_ear=[0, 0, 0])
+        in_pano, _ = pers2pano(in_img, pano_res, vfov=90, rotation_ear=[0, 0, 0])
+        _, in_mask = pers2pano(in_img, pano_res, vfov=88, rotation_ear=[0, 0, 0])
+        in_mask[in_mask <= 0.5] = 0
+        in_mask[in_mask > 0.5] = 1
+        in_mask = torch.from_numpy(in_mask).float().cuda()[..., None]
+
         in_pano = to_norm_tensor(in_pano)
         in_img = cv.resize(in_img, (224, 224))
         in_img = to_norm_tensor(in_img).permute(2, 0, 1)
@@ -134,6 +127,7 @@ if __name__ == '__main__':
         in_asg_path = [p.as_posix() for p in pathlib.Path(asg_paths).glob(f'*{in_img_name}*')][0]
         in_asg = np.array(Image.open(in_asg_path).convert("RGB").resize(pano_res)) / 255.0
         in_asg = to_norm_tensor(in_asg)
+        in_asg = in_asg * (1 - in_mask) + in_pano * in_mask
         in_asg = torch.concat((in_asg, 1 - in_mask), dim=-1)
 
         in_sg_path = [p.as_posix() for p in pathlib.Path(sg_paths).glob(f'*{in_img_name}*')][0]
@@ -142,4 +136,4 @@ if __name__ == '__main__':
         in_sg = np.sum(in_sg * lum_weight, axis=-1, keepdims=True)
         in_sg = torch.from_numpy(in_sg).float().cuda() - 1
 
-        run_sampler(cmodel, in_pano, in_asg, in_sg, in_img, save_name, save_path, input_mask=in_mask, seed=3407)
+        run_sampler(cmodel, in_pano, in_asg, in_sg, in_img, save_name, save_path, seed=3407)
